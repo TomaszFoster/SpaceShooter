f3fc9e9b0dd24f765417258b8e965972750511ed
Tomasz Foster <tomasz@flickerbox.com>
2016-10-27 12:39:51 -0400

    Added audio components and scoring system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,18 +20,20 @@ public class PlayerController : MonoBehaviour {
 	public float fireRate;
 
 	private float nextFire;
-
+	private AudioSource audioSource;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
 	}
 
 	void Update()
 	{
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);// as GameObject;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			audioSource.Play ();
 		}
 	}
 
